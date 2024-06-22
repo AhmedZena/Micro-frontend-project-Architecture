a599d7ff1dac07f9a800414d7fa744a70735a6aa
ahmed zena <ahmedzena53@gmail.com>
2024-06-22 17:29:10 +0300

    prepare wepack for marketing

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,12 +1,21 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const pkgJson = require("../package.json");
 const commonConfig = require("./webpack.common");
 
 const prodConfig = {
   mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: pkgJson.dependencies,
     }),
   ],
 };
