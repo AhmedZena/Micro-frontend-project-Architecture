ada5adab9518fa6acfc706e713b51a9281f585d4
ahmed zena <ahmedzena53@gmail.com>
2024-07-05 16:00:12 +0300

    header component

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import MarketingWebApp from "./components/MarketingWebApp";
+import Header from "./components/Header";
+
+import { BrowserRouter } from "react-router-dom";
 
 export default () => {
-    return <div>
-        <h1>Hey There! from container </h1>
-            <MarketingWebApp />
-    </div>
-
-}
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        {/* <h1>Hey There! from container </h1> */}
+        <MarketingWebApp />
+      </div>
+    </BrowserRouter>
+  );
+};
