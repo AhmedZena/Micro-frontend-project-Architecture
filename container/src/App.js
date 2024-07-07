d13566f5bf1cc71e648832ed68aad438290fa984
ahmed zena <ahmedzena53@gmail.com>
2024-07-07 08:55:58 +0300

    enhance in separate styling

--- container/src/App.js
+++ container/src/App.js
@@ -4,14 +4,27 @@ import Header from "./components/Header";
 
 import { BrowserRouter } from "react-router-dom";
 
+// styling 
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
+
 export default () => {
   return (
     <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
       <div>
         <Header />
         {/* <h1>Hey There! from container </h1> */}
         <MarketingWebApp />
       </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
 
 import Landing from "./components/Landing"
 import Pricing from "./components/Pricing";
+import { StylesProvider , createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "ma"
+})          
+
+
+
 
 export default () =>{
     return (
         <BrowserRouter>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <Switch>
                     <Route exact path="/" component={Landing}/>
                     <Route exact path="/pricing" component={Pricing}/>
