750d604de20834949fda859ae350a24db927b6e6
ahmed zena <ahmedzena53@gmail.com>
2024-07-09 12:02:37 +0300

    marketing chgs

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -3,24 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 // custom history object
-import { createMemoryHistory, createBrowserHistory } from "history";
-if (process.env.NODE_ENV !== "production") {
-  console.log("Looks like we are in development mode!");
-} else {
-  console.log("Looks like we are in production mode!");
-}
+import { createMemoryHistory , createBrowserHistory } from "history";
+ if (process.env.NODE_ENV !== "production") {
+   console.log("Looks like we are in development mode!");
+ } else {
+    console.log("Looks like we are in production mode!");     
+  }
 
 // mount funct to start the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-  console.log(defaultHistory, onNavigate);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  console.log(defaultHistory , onNavigate);
   // create a custom history object
-  const history = defaultHistory || createMemoryHistory({
-    initialEntries: [initialPath],
-  }); //custom history object that is used to navigate between different routes
-  // onNavigate && history.listen(onNavigate); // that listenning to path and when change it work
-  if (onNavigate) {
-    history.listen(onNavigate);
-  }
+  const history =defaultHistory || createMemoryHistory();  //custom history object that is used to navigate between different routes
+  onNavigate && history.listen(onNavigate); // that listenning to path and when change it work
 
   ReactDOM.render(<App history={history} />, el);
 
@@ -42,7 +37,7 @@ if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
 
   if (el) {
-    mount(el, { defaultHistory: createBrowserHistory() });
+    mount(el, {defaultHistory:createBrowserHistory()});
   }
 }
 
